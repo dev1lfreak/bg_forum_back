b6ca32001d565043601e1a2f5312e333a85fae03
dev1lfreak <kvachuk.sergey@yandex.ru>
2026-04-13 18:29:23 +0300

    fix : problems with posting

--- src/modules/comments/models/comment.inputs.ts
+++ src/modules/comments/models/comment.inputs.ts
@@ -1,20 +1,29 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsInt, IsString, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateCommentInput {
   @Field()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(1000)
   text: string;
 
   @Field(() => Int)
+  @IsInt()
   postId: number;
 }
 
 @InputType()
 export class UpdateCommentInput {
   @Field(() => Int)
+  @IsInt()
   id: number;
 
   @Field()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(1000)
   text: string;
 }
 
--- src/modules/posts/models/post.inputs.ts
+++ src/modules/posts/models/post.inputs.ts
@@ -2,37 +2,59 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { Status } from '@prisma/client';
 import { PostSortField } from '../dto/find-all-posts.dto';
 import { SortOrderGql } from './post-sort.model';
+import { IsArray, IsEnum, IsInt, IsOptional, IsString, MinLength } from 'class-validator';
 
 @InputType()
 export class CreatePostInput {
   @Field()
+  @IsString()
+  @MinLength(1)
   title: string;
 
   @Field()
+  @IsString()
+  @MinLength(1)
   content: string;
 
   @Field(() => Status, { nullable: true })
+  @IsOptional()
+  @IsEnum(Status)
   status?: Status;
 
   @Field(() => [Int], { nullable: true })
+  @IsOptional()
+  @IsArray()
+  @IsInt({ each: true })
   tagIds?: number[];
 }
 
 @InputType()
 export class UpdatePostInput {
   @Field(() => Int)
+  @IsInt()
   id: number;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
   title?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
   content?: string;
 
   @Field(() => Status, { nullable: true })
+  @IsOptional()
+  @IsEnum(Status)
   status?: Status;
 
   @Field(() => [Int], { nullable: true })
+  @IsOptional()
+  @IsArray()
+  @IsInt({ each: true })
   tagIds?: number[];
 }
 
